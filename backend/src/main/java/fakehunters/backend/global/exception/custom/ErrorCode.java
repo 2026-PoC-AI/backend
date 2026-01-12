48f557bd3a161a204cd523b3536cdd78b8827f56
KimSoHee <118681718+BillionDollarSohee@users.noreply.github.com>
2026-01-12 17:34:25 +0900

    refactor: 중복된 폴더 구조 정리 및 소스 코드 위치 변경

--- backend/src/main/java/fakehunters/backend/global/exception/custom/ErrorCode.java
+++
@@ -1,9 +0,0 @@
-package fakehunters.backend.global.exception.custom;
-
-import org.springframework.http.HttpStatus;
-
-public interface ErrorCode {
-    String getCode();
-    String getMessage();
-    HttpStatus getStatus();
-}
