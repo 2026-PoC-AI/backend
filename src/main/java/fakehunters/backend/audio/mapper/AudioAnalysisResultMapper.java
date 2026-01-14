808f63481d9153746771bf1f0353b51f5360094b
so0126 <nada981226@gmail.com>
2026-01-14 12:15:24 +0900

    chore: db mybatis 테스트 위한 컨트롤러, 서비스 추가 및 필요한 파일 수정

--- src/main/java/fakehunters/backend/audio/mapper/AudioAnalysisResultMapper.java
+++ src/main/java/fakehunters/backend/audio/mapper/AudioAnalysisResultMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface AudioAnalysisResultMapper {
 
-    int insert(AudioAnalysisResultInsertParam param);
+    long insert(AudioAnalysisResultInsertParam param);
 
     AudioAnalysisResult findById(@Param("id") long id);
 }
